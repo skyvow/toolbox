c9e375912b95b9256b1435a8d57809a93a56667e
skyvow <724355924@qq.com>
2015-11-20 22:13:15 +0800

    add loading

--- src/scripts/custom/app.js
+++ src/scripts/custom/app.js
@@ -23,10 +23,29 @@ App.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', '$locationP
     // true is the default, but I left this here as an example:
     cfpLoadingBarProvider.includeSpinner = true;
     // cfpLoadingBarProvider.includeBar = false;
-    // cfpLoadingBarProvider.spinnerTemplate = '<div class="test"><div id="loading-bar-spinner"><div class="spinner-icon"></div></div></div>';
+    // cfpLoadingBarProvider.spinnerTemplate = '<div class="load-main-box"><div class="load-box"><div class="logo"><span></span></div><span id="text" loading>Sky vow</span></div></div>';
     
+    $httpProvider.interceptors.push('timestampMarker');
 }]);
 
 App.run(['$rootScope', function($rootScope){
     $rootScope.sidebarActive = false;
+}]);
+
+App.factory('timestampMarker', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
+    var timestampMarker = {
+        request: function (config) {
+            $rootScope.loading = true;
+            config.requestTimestamp = new Date().getTime();
+            return config;
+        },
+        response: function (response) {
+            $timeout(function(){
+                $rootScope.loading = false;
+            }, 3000);
+            response.config.responseTimestamp = new Date().getTime();
+            return response;
+        }
+    };
+    return timestampMarker;
 }]);
